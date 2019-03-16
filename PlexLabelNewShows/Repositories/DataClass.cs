90894d79cbe739a33d4b89afa02bed7eb6cf96ee
Justin Price <pricejt@gmail.com>
2019-03-15 22:07:36 -0400

    Initial Push

---
+++ PlexLabelNewShows/Repositories/DataClass.cs
@@ -0,0 +1,67 @@
+﻿using System.Data.SQLite;
+using System.Configuration;
+using System.Data;
+
+namespace PlexLabelNewShows.Repositories
+{
+    class DataClass
+    {
+        private SQLiteConnection sqlite;
+        private static readonly string PlexDbPath = ConfigurationManager.AppSettings["PlexDB"];
+
+        public DataClass()
+        {
+            var pathToFile = "Data Source=" + PlexDbPath;
+            sqlite = new SQLiteConnection(pathToFile);
+        }
+
+        public DataTable GetShowsWithoutLabel()
+        {
+            SQLiteDataAdapter ad;
+            DataTable dt = new DataTable();
+
+            const string query = @"SELECT mi.id
+	,title
+	,mi.Studio
+	,round(mi.rating,2) as rating
+	,mi.tags_genre
+	,added_at
+	,episodes.EpisodeCount
+FROM metadata_items mi
+LEFT JOIN (SELECT series.id, Count(*) as EpisodeCount
+	From metadata_items episode 
+	JOIN metadata_items season ON season.id = episode.parent_id 
+	JOIN metadata_items series ON series.id = season.parent_id 
+	Where episode.metadata_type = 4
+	Group by series.id) episodes on
+episodes.id = mi.id
+WHERE mi.library_section_id = 1
+	AND mi.parent_id IS NULL
+	AND NOT EXISTS (
+		SELECT tags.tag
+		FROM tags
+			,taggings
+		WHERE tags.id = taggings.tag_id
+			AND tags.tag_type = 11
+			AND taggings.metadata_item_id = mi.id
+		)
+		AND Cast((JulianDay('now') - julianday(added_at)) as INTEGER) <= 60
+ORDER BY added_at DESC";
+
+            try
+            {
+                sqlite.Open();
+                var cmd = sqlite.CreateCommand();
+                cmd.CommandText = query;
+                ad = new SQLiteDataAdapter(cmd);
+                ad.Fill(dt);
+            }
+            catch (SQLiteException)
+            {
+                //Ignore for now
+            }
+            sqlite.Close();
+            return dt;
+        }
+    }
+}

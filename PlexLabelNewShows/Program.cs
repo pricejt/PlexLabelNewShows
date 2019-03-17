6c61bb0f3e8871ba5c18b213f4905788d5e6b7d2
Justin Price <pricejt@gmail.com>
2019-03-17 14:11:27 -0400

    Update Plex and Email

--- PlexLabelNewShows/Program.cs
+++ PlexLabelNewShows/Program.cs
@@ -9,7 +9,16 @@ namespace PlexLabelNewShows
         {
             var dataclass = new DataClass();
             var dt = dataclass.GetShowsWithoutLabel();
-            dataclass.DisplayResults(dt);
+            if (dt.Rows.Count > 0)
+            {
+                dataclass.DisplayResults(dt);
+                dataclass.MarkShows(dt);
+                dataclass.EmailShows(dt);
+            }
+            else
+            {
+                Console.WriteLine("No Rows to Process");
+            }
             Console.WriteLine("Press enter to close...");
             Console.ReadLine();
         }
--- PlexLabelNewShows/Repositories/DataClass.cs
+++ PlexLabelNewShows/Repositories/DataClass.cs
@@ -2,13 +2,21 @@
 using System.Data.SQLite;
 using System.Configuration;
 using System.Data;
+using System.Net.Mail;
+using static System.DateTime;
 
 namespace PlexLabelNewShows.Repositories
 {
-    class DataClass
+    internal class DataClass
     {
         private static SQLiteConnection sqlite;
         private static readonly string PlexDbPath = ConfigurationManager.AppSettings["PlexDB"];
+        private static readonly string PlexLabel = ConfigurationManager.AppSettings["Label"];
+        private static readonly string FromEmail = ConfigurationManager.AppSettings["FromEmail"];
+        private static readonly string ToEmail = ConfigurationManager.AppSettings["ToEmail"];
+        private static readonly string SMTP = ConfigurationManager.AppSettings["SMTP"];
+        private static readonly string Password = ConfigurationManager.AppSettings["Password"];
+        private static readonly string EmailSubject = ConfigurationManager.AppSettings["EmailSubject"];
 
         public DataClass()
         {
@@ -74,7 +82,7 @@ ORDER BY added_at DESC";
         public void DisplayResults(DataTable dt)
         {
             Console.WriteLine("Rows: " + dt.Rows.Count);
-            
+
             foreach (DataRow dataRow in dt.Rows)
             {
                 foreach (var item in dataRow.ItemArray)
@@ -82,6 +90,103 @@ ORDER BY added_at DESC";
                     Console.WriteLine(item);
                 }
             }
+        }
+
+        public void MarkShows(DataTable dt)
+        {
+            var tag_id = GetTagId(PlexLabel);
+            var now = UtcNow;
+            const string query = @"insert into taggings (metadata_item_id, tag_id, created_at, 'index') values (@item_id, @tag_id, @created, 0);";
+
+
+            if (tag_id != -1)
+            {
+                try
+                {
+                    sqlite.Open();
+
+                    foreach (DataRow dataRow in dt.Rows)
+                    {
+                        var meta_id = dataRow[0];
+                        var cmd = sqlite.CreateCommand();
+                        cmd.Parameters.AddWithValue("@item_id", meta_id);
+                        cmd.Parameters.AddWithValue("@tag_id", tag_id);
+                        cmd.Parameters.AddWithValue("@created", now);
+                        cmd.CommandText = query;
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (SQLiteException ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                    Console.WriteLine("Press enter to close...");
+                    Console.ReadLine();
+                }
+                finally
+                {
+                    sqlite.Close();
+                }
+
+            }
+        }
+
+        public int GetTagId(string label)
+        {
+            const string query = @"select id from tags where tag=@label and tag_type=11";
+            object tagId = null;
+            try
+            {
+                sqlite.Open();
+                var cmd = sqlite.CreateCommand();
+                cmd.Parameters.AddWithValue("@label", label);
+                cmd.CommandText = query;
+                tagId = cmd.ExecuteScalar();
+            }
+            catch (SQLiteException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                Console.WriteLine("Press enter to close...");
+                Console.ReadLine();
+            }
+            finally
+            {
+                sqlite.Close();
+            }
+            if (tagId != null)
+                return Convert.ToInt32(tagId);
+            return -1;
+        }
+
+        public void EmailShows(DataTable dt)
+        {
+            string textBody = " <table border=" + 1 + " cellpadding=" + 0 + " cellspacing=" + 0 + " width = " + 800 + "><tr bgcolor='#4da6ff'><td><b>Id</b></td> <td> <b> Title</b> </td><td> <b> Studio</b> </td><td> <b> Rating</b> </td><td> <b> Genre</b> </td><td> <b> Added</b> </td><td> <b> Episode Count</b> </td></tr>";
+            for (int loopCount = 0; loopCount < dt.Rows.Count; loopCount++)
+            {
+                textBody += "<tr><td>" + dt.Rows[loopCount]["id"] + "</td><td> " + dt.Rows[loopCount]["title"] + "</td><td> " + dt.Rows[loopCount]["studio"] + "</td><td> " + dt.Rows[loopCount]["rating"] + "</td><td> " + dt.Rows[loopCount]["tags_genre"] + "</td><td> " + dt.Rows[loopCount]["added_at"] + "</td><td> " + dt.Rows[loopCount]["EpisodeCount"] + "</td> </tr>";
+            }
+            textBody += "</table>";
+            MailMessage mail = new MailMessage();
+            System.Net.Mail.SmtpClient SmtpServer = new SmtpClient(SMTP);
+
+            mail.From = new MailAddress(FromEmail);
+
+            foreach (var email in ToEmail.Split(';'))
+            {
+                mail.To.Add(email);
+            }
+            mail.Subject = EmailSubject;
+            mail.Body = textBody;
+            mail.IsBodyHtml = true;
+            SmtpServer.Port = 587;
+            SmtpServer.DeliveryMethod = SmtpDeliveryMethod.Network;
+            SmtpServer.UseDefaultCredentials = false;
+            SmtpServer.Credentials = new System.Net.NetworkCredential(FromEmail, Password);
+            SmtpServer.EnableSsl = true;
+            System.Net.ServicePointManager.ServerCertificateValidationCallback = delegate {
+                return true;
+            };
+
+            SmtpServer.Send(mail);
         }
     }
 }
